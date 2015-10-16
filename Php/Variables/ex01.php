0c8ae46aebd8c0087fb212f721b7cfe13f2bbeb7
Juan Diego Pérez <juand@superganadero.com>
2015-10-16 20:20:59 +0200

    Added Var examples

---
+++ Php/Variables/ex01.php
@@ -0,0 +1,28 @@
+<!--
+    Author: Juan Diego Pérez @pekechis
+    E-mail: contact@jdperez.es
+    Description: STARTING TO PLAY WITH PHP VARS
+    Date: October 2015
+    Reference:
+-->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>STARTING TO PLAY WITH PHP VAR</title>
+  </head>
+  <body>
+      <?php
+          //CREATING A SINGLE PHP VAR
+          // $varname
+          $saludo="HOLA";
+
+          //USING THE VAR
+          echo "Using \$saludo var<br>";
+          echo $saludo."<br>";
+
+          
+      ?>
+  </body>
+</html>
