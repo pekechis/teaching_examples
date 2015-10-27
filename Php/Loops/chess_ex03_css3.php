4a8e262e051b81dee423f0d055366851197ad774
Juan Diego Pérez <juand@superganadero.com>
2015-10-27 22:07:01 +0100

    Add Chess version (loops) using CSS3 Flex Elements

---
+++ Php/Loops/chess_ex03_css3.php
@@ -0,0 +1,44 @@
+<!--
+    Author: Juan Diego Pérez @pekechis
+    E-mail: contact@jdperez.es
+    Description: Creating a CHESS using PHP Loops AND CSS3
+    Date: October
+    Reference: http://php.net/manual/en/control-structures.while.php
+    http://php.net/manual/en/control-structures.do.while.php
+    http://php.net/manual/en/control-structures.for.php
+    Requires: HTML & CSS
+-->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CHESS BOARD WITH PHP LOOPS AND CSS3</title>
+    <link rel="stylesheet" type="text/css" href="style_chess_css3.css">
+  </head>
+  <body>
+        <!-- MAIN CONTAINER - CENTERED USING CSS -->
+        <div id="main">
+          <?php
+              //SIZE OF THE CHESS BOARD
+              $tam=8;
+              for ($i=0;$i<$tam;$i++) {
+                  echo "<div class=\"row\">";
+                  //CREATING THE ROW
+                  for ($j=0;$j<$tam;$j++) {
+                     //IF THE FIRST CELL ON THE UPPER RIGHT CORNER IS WHITE AND
+                     //POSITION 0,0
+                     //BLACK CELLS ARE THOSE WITH ODD $i+$j
+                     if ((($i+$j)%2)==1) {
+                       $black="black";
+                     } else {
+                       $black="";
+                     }
+                     echo "<div class=\"cell $black \"></div>";
+                  }
+                  echo "</div>";
+              }
+          ?>
+        </div>
+  </body>
+</html>
