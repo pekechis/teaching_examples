44fe99a63064986ff6724e1c9cca2cdd4c3d8690
Juan Diego Pérez <juand@superganadero.com>
2015-11-06 08:26:41 +0100

    Passing Info examples finished

--- Php/Passing_Info/post_single.php
+++ Php/Passing_Info/post_single.php
@@ -1,30 +1,60 @@
+<!--
+    Author: Juan Diego Pérez @pekechis
+    E-mail: contact@jdperez.es
+    Description: Passing info using POST and HTML forms
+                 using the same file
+    Date: November 2015
+    Reference: http://www.w3schools.com/tags/tag_form.asp
+               http://www.w3schools.com/tags/tag_input.asp
+               http://php.net/manual/reserved.variables.post.php
+-->
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Passing info with POST and HTML FORMS using a single file.</title>
     <link rel="stylesheet" type="text/css" href=" ">
+    <style>
+      span {
+        width: 100px;
+        display: inline-block;
+      }
+    </style>
   </head>
   <body>
-    <?php if (isset($_POST["correo"])) : ?>
 
-      SI QUE HAS MANDADO DATOS
-      <?php var_dump($_POST)  ?>
+      <!-- PHP STRUCTURE FOR CONDITIONAL HTML -->
+      <!-- FIRST TIME. NO DATA IN THE POST (checking a required form field) -->
+      <!-- So we must show the form -->
 
-  <?php else: ?>
-    <form action="post_single.php" method="post">
-      Nombre:<input type="text" name="nombre" required><br>
-      Apellidos:<input type="text" name="apellidos" required><br>
-      Email:<input type="email" name="correo" required><br>
-      Fecha Nacimiento:<input type="date" name="fecha"><br>
-      Edad:<input type="range" min="18" max="25">
-      <input type="submit" value="ENVIAR"><br>
+      <?php if (!isset($_POST["mail"])) : ?>
+        <form method="post">
+          <fieldset>
+            <legend>Personal Info</legend>
+            <span>Name:</span><input type="text" name="name" required><br>
+            <span>Last Name:</span><input type="text" name="lastname" required><br>
+            <span>Email:</span><input type="email" name="mail" required><br>
+            <span>Date if Birth:</span><input type="date" name="date"><br>
+            <span>Sex: </span><input type="radio" name="sex" value="H">Male<input type="radio" name="sexo" value="M">Female<br>
+            <h3>Subjects</h3>
+            <input type="checkbox" name="subjects[]" value="LM">MarkUp Languages<br>
+            <input type="checkbox" name="subjects[]" value="GBD">Database Management<br>
+            <input type="checkbox" name="subjects[]" value="PAR">Computer Networks<br>
+            <input type="checkbox" name="subjects[]" value="IAW">Web Apps<br>
+            <p><input type="submit" value="Send"></p>
+          </fieldset>
+        </form>
 
-    </form>
+      <!-- DATA IN $_POST['mail']. Coming from a form submit -->
+      <?php else: ?>
 
+        <?php
+            echo "<h3>Showing data coming from the form</h3>";
+            var_dump($_POST);
+        ?>
 
-  <?php endif ?>
+      <?php endif ?>
 
   </body>
 </html>
--- Php/Passing_Info/post_source.php
+++ Php/Passing_Info/post_source.php
@@ -1,25 +1,45 @@
+<!--
+    Author: Juan Diego Pérez @pekechis
+    E-mail: contact@jdperez.es
+    Description: Passing info using POST between two pages using HTML forms
+                  This is the source file
+    Date: November 2015
+    Reference: http://www.w3schools.com/tags/tag_form.asp
+               http://www.w3schools.com/tags/tag_input.asp
+               http://php.net/manual/reserved.variables.post.php
+-->
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-    <link rel="stylesheet" type="text/css" href=" ">
+    <title>Passing info using POST and HTML FORMS.SOURCE FILE</title>
+    <style>
+      span {
+        width: 100px;
+        display: inline-block;
+      }
+    </style>
   </head>
   <body>
+      <!-- FORM -->
+      <!-- As a rule we always use method="post" -->
+      <!-- action="post_destinationt" indicates the url where we are sending form data -->
       <form action="post_destination.php" method="post">
-        Nombre:<input type="text" name="nombre" required><br>
-        Apellidos:<input type="text" name="apellidos" required><br>
-        Email:<input type="email" name="correo" required><br>
-        Fecha Nacimiento:<input type="date" name="fecha"><br>
-        Sexo: <input type="radio" name="sexo" value="H">Hombre<br>
-        <input type="radio" name="sexo" value="M">Mujer<br>
-        <input type="checkbox" name="asignaturas[]" value="LM">LM<br>
-        <input type="checkbox" name="asignaturas[]" value="GBD">GBD<br>
-        <input type="checkbox" name="asignaturas[]" value="PAR">PAR<br>
-        <input type="checkbox" name="asignaturas[]" value="IAW">IAW<br>
-        <input type="submit" value="ENVIAR"><br>
-
+        <fieldset>
+          <legend>Personal Info</legend>
+          <span>Name:</span><input type="text" name="name" required><br>
+          <span>Last Name:</span><input type="text" name="lastname" required><br>
+          <span>Email:</span><input type="email" name="mail" required><br>
+          <span>Date if Birth:</span><input type="date" name="date"><br>
+          <span>Sex: </span><input type="radio" name="sex" value="H">Male<input type="radio" name="sexo" value="M">Female<br>
+          <h3>Subjects</h3>
+          <input type="checkbox" name="subjects[]" value="LM">MarkUp Languages<br>
+          <input type="checkbox" name="subjects[]" value="GBD">Database Management<br>
+          <input type="checkbox" name="subjects[]" value="PAR">Computer Networks<br>
+          <input type="checkbox" name="subjects[]" value="IAW">Web Apps<br>
+          <p><input type="submit" value="Send"></p>
+        </fieldset>
       </form>
   </body>
 </html>
