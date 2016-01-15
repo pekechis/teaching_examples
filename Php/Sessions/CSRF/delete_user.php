c8ad958dcc904aad15e01557f3d3a9972db50540
Juan Diego Pérez <juand@superganadero.com>
2016-01-15 07:12:16 +0100

    CSRF Example

---
+++ Php/Sessions/CSRF/delete_user.php
@@ -0,0 +1,52 @@
+<?php
+
+  //Open the session
+  session_start();
+
+  if (!isset($_SESSION["user"])) {
+
+    //User not logged
+    session_destroy();
+    header("Location: login.php");
+  }
+
+  //Already logged
+  if (isser($_GET["id"])) {
+
+    //CREATING THE CONNECTION
+    $connection = new mysqli("localhost", "tf", "12345", "usuarios");
+
+    //TESTING IF THE CONNECTION WAS RIGHT
+    if ($connection->connect_errno) {
+        printf("Connection failed: %s\n", $connection->connect_error);
+        exit();
+    }
+
+    //BUILDING THE DELETE  QUERY
+    $query = $connection->prepare("DELETE * FROM usuarios
+      WHERE id=?");
+
+    //Binding the ? to the post values
+    $query->bind_param("s",$_GET["id"]);
+
+    if ($query->execute()) {
+
+        //No rows returned
+        if ($query->affected_rows===0) {
+          echo "No user deleted";
+        } else {
+
+          echo "User deleted";
+        }
+
+    } else {
+      echo "Wrong Query";
+
+    }
+
+  } else {
+
+    echo "No element selectd";
+  }
+
+ ?>
---
+++ Php/Sessions/CSRF/login.php
@@ -0,0 +1,84 @@
+<!--
+    Author: Juan Diego Pérez @pekechis
+    E-mail: contact@jdperez.es
+    Description: LOGIN WITH PREPARED STATEMENTS. NOT VULNERABLE TO SQL
+                 INJECTION
+    Date: January 2016
+    Reference: http://php.net/manual/en/mysqli.quickstart.prepared-statements.php
+-->
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href=" ">
+  </head>
+  <body>
+
+    <?php
+        //FORM SUBMITTED
+        if (isset($_POST["user"])) {
+
+          //CREATING THE CONNECTION
+          $connection = new mysqli("localhost", "tf", "12345", "usuarios");
+
+          //TESTING IF THE CONNECTION WAS RIGHT
+          if ($connection->connect_errno) {
+              printf("Connection failed: %s\n", $connection->connect_error);
+              exit();
+          }
+
+          //MAKING A SELECT QUERY
+          //Password coded with md5 at the database. Look for better options
+
+          //SHOWING THE UNSAFE VERSION -- SQL INJECTION POSSIBLE
+          //$consulta="select * from usuarios where
+          //username='".$_POST["user"]."' and password=md5('".$_POST["password"]."');";
+          $query = $connection->prepare("SELECT * FROM usuarios
+            WHERE username=? AND password=md5(?)");
+
+          //Binding the ? to the post values
+          $query->bind_param("ss",$_POST["user"],$_POST["password"]);
+
+
+          //Test if the query was correct
+          //SQL Injection Possible
+          //Check http://php.net/manual/es/mysqli.prepare.php for more security
+          if ($query->execute()) {
+
+              //No rows returned
+              if ($query->affected_rows===0) {
+                echo "LOGIN INVALIDO";
+              } else {
+                //VALID LOGIN. SETTING SESSION VARS
+                $_SESSION["user"]=$_POST["user"];
+                $_SESSION["language"]="es";
+
+                //Redirecting to the Start Page
+                header("Location: index.php");
+              }
+
+          } else {
+            echo "Wrong Query";
+
+            var_dump($consulta);
+          }
+      }
+    ?>
+
+    <form action="login.php" method="post">
+
+      <p><input name="user" required></p>
+      <p><input name="password" type="password" required></p>
+      <p><input type="submit" value="Log In"></p>
+
+    </form>
+
+
+
+  </body>
+</html>
