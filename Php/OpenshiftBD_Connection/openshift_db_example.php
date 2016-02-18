ed28c5113916a9b4118dbd1759dac8fac9d60ae2
Juan Diego Pérez <juand@superganadero.com>
2016-02-18 17:43:38 +0100

    Openshift db example added

---
+++ Php/OpenshiftBD_Connection/openshift_db_example.php
@@ -0,0 +1,75 @@
+<!--
+    Author: Juan Diego Pérez @pekechis
+    E-mail: contact@jdperez.es
+    Description: Using
+    Date: February 2016
+    Reference:
+-->
+<?php
+  include_once("./db_configuration.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DB Example Using OpenShift DB Configuration</title>
+  </head>
+  <body>
+    <?php
+
+      //CREATING THE CONNECTION
+      $connection = new mysqli("localhost", "tf", "12345", "TalleresFaber");
+
+      //TESTING IF THE CONNECTION WAS RIGHT
+      if ($connection->connect_errno) {
+          printf("Connection failed: %s\n", $connection->connect_error);
+          exit();
+      }
+
+      //MAKING A SELECT QUERY
+      /* Consultas de selección que devuelven un conjunto de resultados */
+      if ($result = $connection->query("SELECT * FROM CLIENTES;")) {
+
+          printf("<p>The select query returned %d rows.</p>", $result->num_rows);
+
+      ?>
+
+          <!-- PRINT THE TABLE AND THE HEADER -->
+          <table style="border:1px solid black">
+          <thead>
+            <tr>
+              <th>CodCliente</th>
+              <th>Nombre</th>
+              <th>Apellidos</th>
+              <th>DNI</th>
+              <th>Direccion</th>
+              <th>Teléfono</th>
+          </thead>
+
+      <?php
+
+          //FETCHING OBJECTS FROM THE RESULT SET
+          //THE LOOP CONTINUES WHILE WE HAVE ANY OBJECT (Query Row) LEFT
+          while($obj = $result->fetch_object()) {
+              //PRINTING EACH ROW
+              echo "<tr>";
+              echo "<td>".$obj->CodCliente."</td>";
+              echo "<td>".$obj->Nombre."</td>";
+              echo "<td>".$obj->Apellidos."</td>";
+              echo "<td>".$obj->DNI."</td>";
+              echo "<td>".$obj->Direccion."</td>";
+              echo "<td>".$obj->Telefono."</td>";
+              echo "</tr>";
+          }
+
+          //Free the result. Avoid High Memory Usages
+          $result->close();
+          unset($obj);
+          unset($connection);
+
+      } //END OF THE IF CHECKING IF THE QUERY WAS RIGHT
+
+    ?>
+  </body>
+</html>
