d72d851182e2a9032adaa2cd3e7443ea5474a0ff
Juan Diego Pérez <juand@superganadero.com>
2016-12-01 10:06:43 +0100

    Funcion Examples December 2016

--- Php/Functions/library.php
+++ Php/Functions/library.php
@@ -17,4 +17,17 @@ function array_average($v) {
 
 }
 
+function longest_string($v) {
+
+  $element=$v[0];
+
+  for ($i=1;$i<count($v);$i++) {
+      if (strlen($element)<strlen($v[$i])) {
+        $element=$v[$i];
+      }
+  }
+
+  return $element;
+}
+
 ?>
---
+++ Php/Functions/longest_string.php
@@ -0,0 +1,27 @@
+<!--
+    Author: Juan Diego Pérez @pekechis
+    E-mail: contact@jdperez.es
+    Description: Find the longest string in an array
+    Date: December 2016
+    Reference: http://php.net/manual/functions.user-defined.php
+-->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FIND THE LONGEST STRING IN AN ARRAY</title>
+    <link rel="stylesheet" type="text/css" href=" ">
+  </head>
+  <body>
+      <?php
+
+        include_once("library.php");
+
+        $cadenas = ["HELLO","GOOBYE","HERE","XXXXXXXXXXX"];
+
+        echo longest_string($cadenas);
+
+      ?>
+  </body>
+</html>
