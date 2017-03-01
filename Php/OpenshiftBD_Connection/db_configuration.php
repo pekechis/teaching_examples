14d56a6837207bdc810b62d378f23534761f37e1
Juan Diego Pérez <juand@superganadero.com>
2017-03-01 09:35:56 +0100

    Contextual Menu

--- Php/OpenshiftBD_Connection/db_configuration.php
+++ Php/OpenshiftBD_Connection/db_configuration.php
@@ -9,11 +9,11 @@
 
 
   //Checking if we are into the OpenShift App
-  if (isset($_ENV['OPENSHIFT_APP_NAME'])) {
-    $db_user=$_ENV['OPENSHIFT_MYSQL_DB_USERNAME']; //Openshift db name OPENSHIFT_MYSQL_DB_USERNAME
-    $db_host=$_ENV['OPENSHIFT_MYSQL_DB_HOST']; //Openshift db host OPENSHIFT_MYSQL_DB_HOST
-    $db_password=$_ENV['OPENSHIFT_MYSQL_DB_PASSWORD']; //Openshift db password OPENSHIFT_MYSQL_DB_PASSWORD
-    $db_name="tf"; //Openshift db name
+  if (isset($_ENV['MYSQL_USER'])) {
+    $db_user=$_ENV['MYSQL_USER']; //Openshift db name OPENSHIFT_MYSQL_DB_USERNAME
+    $db_host=$_ENV['MYSQL_HOST']; //Openshift db host OPENSHIFT_MYSQL_DB_HOST
+    $db_password=$_ENV['MYSQL_PASSWORD']; //Openshift db password OPENSHIFT_MYSQL_DB_PASSWORD
+    $db_name=$_ENV["MYSQL_DB"]; //Openshift db name
   } else {
     $db_user="tf"; //my db user
     $db_host="localhost"; //my db host
