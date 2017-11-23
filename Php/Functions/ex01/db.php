6c949d69a6b233ab6ae3a319c6d4ae1cdbfcbf7a
Juan Diego Pérez <pekechis@gmail.com>
2017-11-23 10:14:31 +0100

    Conectar

---
+++ Php/Functions/ex01/db.php
@@ -0,0 +1,25 @@
+
+<?php
+
+function conectar($host ="192.168.1.30",
+                  $user = "root",
+                  $password = "Admin2015",
+                  $bd = "tf",
+                  $port = 3316) {
+
+
+  //CREATING THE CONNECTION
+  $connection = new mysqli($host, $user, $password, $bd,$port);
+  $connection->set_charset("uft8");
+
+  //TESTING IF THE CONNECTION WAS RIGHT
+  if ($connection->connect_errno) {
+      echo "Error al conectar";
+      return false;
+  }
+
+  return $connection;
+
+}
+
+?>
